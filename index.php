a6658a63a1340258c27a6dfb49e8d566dd0455d1
Krystian Wawruch <CresixU@gmail.com>
2022-12-08 21:12:03 +0100

    Users show correctly

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         </div>
         <div>
           <div>
-            <input type="text" placeholder="Znajdź kanał">
+            <input type="text" id="searchbox" placeholder="Znajdź kanał">
             <button class="btn-addnew-channel">DODAJ KANAŁ</button>
           </div>
           <table class="table">
@@ -34,7 +34,7 @@
                 <th>Akcje</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody class="table">
               <tr>
                 <td>Nazwa</td>
                 <td>@nazwaid</td>
@@ -73,8 +73,39 @@
           </nav>
         </div>
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script>
+        var myData, newData = [];
+
+        $('#searchbox').on('change', function (){
+        $.ajax({
+            url: `http://162.19.172.197:8080/api/v1/profile?page=0&size=20`,
+        })
+            .done(res => {
+              newData = res.data;
+              console.log(res.data[0]);
+              for(var i=0; i<20;i++) {
+                console.log(myData[i].name);
+                $(".table").append( $('<tr><td>'+myData[i].name+'</td><td></td></tr>') );
+              }
+
+            });
+        })
+
+        $.ajax({
+            url: `http://162.19.172.197:8080/api/v1/profile?page=0&size=20`,
+        })
+            .done(res => {
+              myData = res.data;
+              //console.log(res.data[0]);
+              for(var i=0; i<20;i++) {
+                var channelUniqueName = myData[i].url.substring(myData[i].url.lastIndexOf('/') + 1);
+                $(".table").append( $('<tr><td>'+myData[i].name+'</td><td>'+channelUniqueName+'</td><td><a href="'+myData[i].url+'">'+myData[i].media+'</a></td><td><a href="/streams/profile/'+myData[i].id+'"><i class="bi bi-card-list"></i></a></td><td><a href="#"><i class="bi bi-trash"></i></a></td></tr>') );
+              }
+            });
+
+    </script>
   </body>
 </html>
