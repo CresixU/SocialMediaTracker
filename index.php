d1be30c2da47c6dae5a48ebc0df390affa0a25ec
Krystian Wawruch <CresixU@gmail.com>
2023-02-11 11:25:40 +0100

    Add socket.io

--- index.php
+++ index.php
@@ -64,6 +64,7 @@
         <div>
           <div>
             <input type="text" id="searchbox" placeholder="Znajdź kanał">
+            <div style="float:left;padding:10px 15px;">Status recaptcha: <span id="socket-status"></span> </div>
             <button class="btn-addnew-channel" id="show_modal_new_channel">DODAJ KANAŁ</button>
           </div>
           <table class="table">
@@ -98,6 +99,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdn.socket.io/4.6.0/socket.io.min.js" integrity="sha384-c79GN5VsunZvi+Q/WObgk2in0CbZsHnjEqvFxC5DxHn9lTfNce2WW6h2pH6u/kF+" crossorigin="anonymous"></script>
     <script>
         var myData = [];
         var userDeleteId = null;
@@ -239,6 +241,18 @@
             ShowData(0,size);
           }
         })
+
+        //Recaptcha
+        
+        const socket = new WebSocket("ws://127.0.0.7:7095");
+
+        socket.addEventListener("get_message", ({ data }) => {
+          const packet = JSON.parse(data);
+          $('#socket-status').text(packet.message);
+        });
+        
+
+
     </script>
   </body>
 </html>
